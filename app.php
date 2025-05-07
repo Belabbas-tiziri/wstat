82f7b34314223b3e58392a036a964cad59901034
Belabbas-tiziri <belabbas.tiziri@gmail.com>
2025-05-07 15:42:25 +0200

    Create app.php

---
+++ app.php
@@ -0,0 +1,12 @@
+<?php
+// test.php
+
+// Affiche un message
+echo "<h1>Test PHP Réussi !</h1>";
+
+// Affiche la date et l'heure actuelle
+echo "<p>Date et heure actuelles : " . date("Y-m-d H:i:s") . "</p>";
+
+// Affiche des informations sur PHP
+phpinfo();
+?>
